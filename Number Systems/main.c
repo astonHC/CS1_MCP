fb3621941fed0ad17494477f1fef9da307c70f82
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-09-28 00:04:55 +0100

    added octal function
    which is just a copy and paste of the previous minus a diff in weighting

--- Number Systems/main.c
+++ Number Systems/main.c
@@ -9,6 +9,7 @@
 
 /* SYSTEM INCLUDES */
 
+#include <cstdint>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,6 +17,7 @@
 #pragma warning(disable : 4996)
 
 #define MAX_BINARY_LENGTH              1000
+#define OCTAL_DIGIT_SIZE                  3
 #define HEX_DIGIT_SIZE                    4
 #define BINARY_STR_LENGTH(NUM)  strlen(NUM)
 
@@ -31,6 +33,7 @@ typedef struct BINARY_NUMBER
 
 static BINARY_NUMBER* NUMBER;
 static int HEX_DIGITS = 0;
+static int OCTAL_DIGITS = 0;
 
 /* ASSUMING THAT THE LENGTH OF THE NUMBER IS EQUAL TO THE MAX SIZE */
 /* ALLOCATE MEMORY FOR THE HEX REPRESENTATION */
@@ -40,7 +43,7 @@ static int HEX_DIGITS = 0;
 static void BINARY_TO_HEX(BINARY_NUMBER* NUMBER)
 {
     int LENGTH = BINARY_STR_LENGTH(NUMBER->BINARY);
-    int HEX_DIGITS = LENGTH / HEX_DIGIT_SIZE;
+    HEX_DIGITS += LENGTH / HEX_DIGIT_SIZE;
 
     if (LENGTH % HEX_DIGIT_SIZE != 0) HEX_DIGITS++;
 
@@ -91,15 +94,47 @@ static void INIT_HEX_STRING()
     }
 }
 
+/* CARRY OUT SIMILAR FUNCTIONS AKIN TO THE PREVIOUS FUNCTION CALLS EXCEPT */
+/* EXCEPT FOR AN OCTAL CONVERSION */
+
+static void BINARY_TO_OCTAL(BINARY_NUMBER* NUMBER)
+{
+    int LENGTH = BINARY_STR_LENGTH(NUMBER->BINARY);
+    OCTAL_DIGITS = LENGTH / OCTAL_DIGIT_SIZE; 
+
+    if (LENGTH % OCTAL_DIGIT_SIZE != 0) OCTAL_DIGITS++;
+
+    NUMBER->OCTAL = (char*)malloc(OCTAL_DIGITS + 1);
+
+    if (!NUMBER->OCTAL) {
+        perror("Memory allocation failed");
+        exit(1);
+    }
+
+    for (int i = LENGTH - 1, j = OCTAL_DIGITS - 1; i >= 0; i--, NUMBER->WEIGHT *= 2)
+    {
+        if (NUMBER->BINARY[i] == '1') NUMBER->DIGIT += NUMBER->WEIGHT;
+
+        if (NUMBER->WEIGHT == 8 || i == 0)
+        {
+            NUMBER->OCTAL[j] = '0' + NUMBER->DIGIT;
+
+            NUMBER->DIGIT = 0;
+            NUMBER->WEIGHT = 1;
+            j--;
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
     BINARY_NUMBER NUMBER;
-    NUMBER.BINARY = (char*)malloc(MAX_BINARY_LENGTH);
+    NUMBER.BINARY = (char*)malloc(MAX_BINARY_LENGTH); // ASSUME THAT THERE IS MEMORY TO DYNAMICALLY ALLOCATE
     INIT_HEX_STRING();
 
     if (!NUMBER.BINARY)
     {
-        perror("Lmao you cannot convert this number, there must be a Seg Fault somewhere");
+        perror("Lmao you cannot convert this number, there must be a Seg Fault somewhere"); // SEG FAULT AND OR BUFFER/INTEGER OVERFLOW INTO rdx/rax
         return 1;
     }
 
@@ -108,7 +143,7 @@ int main(int argc, char* argv[])
 
     BINARY_TO_HEX(&NUMBER);
 
-    printf("Hexadecimal Equivalent: %s\n", NUMBER.HEXADECIMAL);
+    printf("Hexadecimal Equivalent: %s", NUMBER.HEXADECIMAL);
 
     free(NUMBER.BINARY);
     free(NUMBER.HEXADECIMAL);
