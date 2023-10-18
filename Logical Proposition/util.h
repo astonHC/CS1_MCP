ad3212e3d0415d5845d2f587f377183a946f4576
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-10-18 20:44:51 +0100

    Add files via upload

---
+++ Logical Proposition/util.h
@@ -0,0 +1,16 @@
+/* COPYRIGHT (C) HARRY CLARK */
+
+/* CS1_MCP Boolean Algebraic Expression Calculator */
+
+/* THIS FILE PERTAINS TO MISCELLANEOUS UTILITY */
+
+#ifndef UTIL
+#define UTIL
+
+#define     AND                       '&'
+#define     OR                        '|'
+#define     NOT                       '!'
+#define     OPEN_PARANTHESIS          '('
+#define     CLOSED_PARANTHESIS        ')'
+
+#endif
