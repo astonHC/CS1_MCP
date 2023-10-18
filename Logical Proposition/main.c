9d55f67c6fa521b6a60dc0ee94bf1ddda2561dce
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-10-18 17:56:38 +0100

    added expression switch

--- Logical Proposition/main.c
+++ Logical Proposition/main.c
@@ -22,28 +22,55 @@
 
 typedef struct EXPRESSION
 {
-    char NAME;
-    bool VALUE;
+    char EXP_BASE;
+    char EXP_NAME;
+    char EXP_OPERAND;
+    bool EXP_VALUE;
+    bool EXP_READ_L;
+    bool EXP_READ_R;
+    size_t EXP_LEN;
 };
 
 #endif
 
 #undef USE_ARGS
 
+struct EXPRESSION* EXP;
+
 /* FIND THE VARIABLE TYPE BY NAME IN ASSOCIATION WITH THE LIST OF EXPRESSIONS */
 /* ASSUMING AN ARBITARY LENGTH OF THE PROPOSITION, EVALUATE IT BASED ON HOW MANY EXPRESSIONS THERE ARE */
 
-static bool FIND_TYPE(struct EXPRESSION* EXP, size_t VAR_COUNT, char VAR_NAME)
+static bool FIND_TYPE(size_t VAR_COUNT, char VAR_NAME)
 {
     for (size_t i = 0; i < VAR_COUNT; i++)
     {
-        if(EXP[i].NAME == VAR_NAME)
+        if(EXP[i].EXP_NAME == VAR_NAME)
         {
-            return EXP->VALUE;
+            return EXP->EXP_VALUE;
         }
     }
 
     return false;   
+}
+
+/* EVALUATE THE EXPRESSION RECURSIVELY IN CONJUNCTION WITH AN ARBITARY LENGTH OF THE DESIRED PROPOSITION */
+
+static bool EVALUAATE_TYPE()
+{
+    switch (EXP->EXP_BASE)
+    {
+    case ' ':
+        return EVALUAATE_TYPE(EXP->EXP_BASE + 1, sizeof(EXP));
+
+    case 'T':
+        return true;
+
+    case 'F':
+        return false;
+
+    default:
+        break;
+    }
 }
 
 #endif
