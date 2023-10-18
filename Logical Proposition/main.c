15c08525e65123f255d11639ab0cfe2dfcc4e3d9
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-10-18 22:31:20 +0100

    Update main.c

--- Logical Proposition/main.c
+++ Logical Proposition/main.c
@@ -4,82 +4,21 @@
 
 /* THIS FILE PERTAINS TO THE MAIN FUNCTIONALITY OF THE PROGRAM */
 
-#ifndef BOOLEAN_CALC
-#define BOOLEAN_CALC
-
 /* NESTED INCLUDES */
 
-#include <util.h>
+#include "util.h"
 
-/* SYSTEM INCLUDES */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
-
-#if defined(USE_ARGS)
-#define USE_ARGS
-#else
-#define USE_ARGS
-
-typedef struct EXPRESSION
+int main(int argc, char* argv[])
 {
-    char EXP_BASE;
-    char EXP_NAME;
-    char EXP_OPERAND;
-    bool EXP_VALUE;
-    bool EXP_READ_L;
-    bool EXP_READ_R;
-    size_t EXP_LEN;
-};
+    struct EXPRESSION* EXP;
 
-#endif
+    // DYNAMICALLY ALLOCATE THE PROPRIATORY MEMORY NEEDED
+    // SUCH THAT THE PROGRAM CAN DETERMINE THE SIZE OF HOW MANY
+    // ARGS ARE PRESENT
 
-#undef USE_ARGS
-
-struct EXPRESSION* EXP;
-
-/* FIND THE VARIABLE TYPE BY NAME IN ASSOCIATION WITH THE LIST OF EXPRESSIONS */
-/* ASSUMING AN ARBITARY LENGTH OF THE PROPOSITION, EVALUATE IT BASED ON HOW MANY EXPRESSIONS THERE ARE */
-
-static bool FIND_TYPE(size_t VAR_COUNT, char VAR_NAME)
-{
-    for (size_t i = 0; i < VAR_COUNT; i++)
+    if(argc != 2)
     {
-        if(EXP[i].EXP_NAME == VAR_NAME)
-        {
-            return EXP->EXP_VALUE;
-        }
-    }
-
-    return false;   
-}
-
-/* EVALUATE THE EXPRESSION RECURSIVELY IN CONJUNCTION WITH AN ARBITARY LENGTH OF THE DESIRED PROPOSITION */
-
-static bool EVALUAATE_TYPE()
-{
-    switch (EXP->EXP_BASE)
-    {
-
-    // BLANK USE CASE FOR ASSUMING ARBITARY EXPRESSION
-
-    case ' ':
-        return EVALUAATE_TYPE(EXP->EXP_BASE + 1, sizeof(EXP));
-
-    case 'T':
-        return true;
-
-    case 'F':
-        return false;
-
-
-
-    default:
-        break;
+        EXP->EXP_COUNT = (size_t*)malloc(sizeof(EXP) / sizeof(EXP[0]));
     }
 }
 
-#endif
