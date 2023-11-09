f2e972b6c1e9e96a122c936c4f892a9e3b5b71ba
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-11-09 18:12:17 +0000

    Update matrix.c

--- Matrices/matrix.c
+++ Matrices/matrix.c
@@ -17,18 +17,38 @@
 
 static MATRIX* MATRIX_OBJ;
 
-static void GET_MATRIX_ELEMENTS()
+/* ESTABLISH THE AMOUNT OF ELEMENTS INSIDE OF THE MATRIX */
+/* BASED ON USER INPUTS */
+
+static void GET_MATRIX_ELEMENTS(int** MATRIX, int ROW, int COLUMN)
 {
+    printf("\n Enter Matrix Elements: /n");
+
     for (int i = 0; i < MATRIX_OBJ->CONTENTS.ROWS; i++)
     {
-        for (int j = 0; i < MATRIX_OBJ->CONTENTS.COLUMNS; j++)
+        for (int j = 0; j < MATRIX_OBJ->CONTENTS.COLUMNS; j++)
         {
             printf("Enter a%d%d: ", i += 1, j += 1);
             scanf("%d", 0, sizeof(MATRIX_OBJ->MATRIX_BASE[i][j]));
         }
         
+    }   
+}
+
+/* MULTIPLY THE CORRESPONDENCE BASED ON THE USER INPUTS */
+
+static void MULTIPLY_MATRICES()
+{
+    for (int i = 0; i < MATRIX_OBJ->CONTENTS.ROWS; i++)
+    {
+        for (int j = 0; j < MATRIX_OBJ->CONTENTS.COLUMNS; j++)
+        {
+            for (int k = 0; k < MATRIX_OBJ->CONTENTS.COLUMNS; k++)
+            {
+                MATRIX_OBJ->RESULT[i][j] += MATRIX_OBJ->MATRIX_1[i][k] * MATRIX_OBJ->MATRIX_2[k][j];
+            }
+        }
     }
-    
 }
 
 #endif
