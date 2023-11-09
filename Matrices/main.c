9716d3bcf22e01837f1a6db12616d10502f57c87
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-11-09 18:13:08 +0000

    Update main.c

--- Matrices/main.c
+++ Matrices/main.c
@@ -10,5 +10,12 @@
 
 int main(void)
 {
-    
+    MATRIX MATRIX;
+
+    printf("Enter rows and columns for Matrix 1");
+    scanf("%d", &MATRIX.CONTENTS.ROWS);
+
+    GET_MATRIX_ELEMENTS(&MATRIX, MATRIX.CONTENTS.ROWS, MATRIX.CONTENTS.COLUMNS);
+    MULTIPLY_MATRICES();
+    DISPLAY_RESULTS();
 }
