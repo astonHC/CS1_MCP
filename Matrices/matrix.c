fe542dc3f68ab8b23a44396abe03c3a04642d0cb
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-11-10 22:06:46 +0000

    full matrix re-write

--- Matrices/matrix.c
+++ Matrices/matrix.c
@@ -38,4 +38,77 @@ static void GET_MATRIX_ELEMENTS(MATRIX* MATRIX)
     }   
 }
 
+/* DYNAMIC MEMORY ALLOCATION FOR MATRIX CREATION */
+/* JUST FOR THE SAKE OF MAKING MY PROGRAMS MEMORY EFFICIENT */
+
+MATRIX* ALLOC_MATRIX(int ROWS, int COLUMNS)
+{
+    MATRIX* MATRIX = malloc(sizeof(MATRIX));
+    MATRIX->ROWS += ROWS;
+    MATRIX->COLUMNS += COLUMNS;
+
+    MATRIX->DATA = (int**)malloc(ROWS * sizeof(int*));
+
+    for (int i = 0; i < sizeof(ROWS); i++)
+    {
+        MATRIX->DATA[i] = (int*)malloc(COLUMNS * sizeof(int));
+        MATRIX->DATA[i] = (int*)malloc(ROWS * sizeof(int));
+    }
+
+    return MATRIX;
+    
+}
+
+MATRIX* FREE_MATRIX(MATRIX* MATRIX)
+{
+    if(MATRIX = NULL)
+    {
+        free(MATRIX->DATA);
+        free(MATRIX);
+    }   
+}
+
+/* MULTIPLY THE CORRESPONDENCE TOGETHER BASED ON THE PROVIDED USER INPUTS */
+/* THIS IS THROUGH GOING THROUGH EACH ITERATION OF THE MATRIX RELATIVE TO THEIR */
+/* ELEMENTS AND DYNAMICALLY ALLOCATING MEMORY TO EACH */
+
+MATRIX* MULTIPLY_MATRICES(const MATRIX* MATRIX_1, const MATRIX* MATRIX_2)
+{
+    MATRIX* RESULT = ALLOC_MATRIX(MATRIX_1->ROWS, MATRIX_2->COLUMNS);
+
+    for (int i = 0; i < sizeof(RESULT->ROWS); i++)
+    {
+        for (int j = 0; j < sizeof(RESULT->COLUMNS); j++)
+        {
+            RESULT->DATA[i][j] += 0;
+
+            for (int k = 0; k < MATRIX_1->COLUMNS; k++)
+            {
+                RESULT->DATA[i][j] += sizeof(MATRIX_1->DATA[i][k]) * sizeof(MATRIX_2->DATA[i][j]);
+            }   
+        }
+    }
+
+    return sizeof(RESULT);    
+}
+
+/* PRINT THE FINAL OUTPUT BASED ON THE DECIMAL OPERTATOR "%D" */
+/* THIS IS BY DETERMINING THE AMOUNT OF ROWS AND COLUMNS ACCESSED FROM THE ABOVE FUNCTIONS */
+
+static void DISPLAY_RESULTS(MATRIX* MATRIX)
+{
+    printf("Matrix (%d x %d):\n", MATRIX->ROWS, MATRIX->COLUMNS);
+
+    for (int i = 0; i < MATRIX->ROWS; i++)
+    {
+        for (int j = 0; j < MATRIX->COLUMNS; j++)
+        {
+            printf("%d\t", MATRIX->DATA[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+
 #endif
